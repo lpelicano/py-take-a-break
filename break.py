6c2ab660d1de9ef465478de4dad1639d31f79aa4
lpelicano <lehpelicano@gmail.com>
2017-05-02 14:36:19 +0100

    complete

--- break.py
+++ break.py
@@ -1,12 +1,12 @@
 import time, webbrowser
 
 
-total_breaks = 3
+total_breaks = 6
 break_count = 0
 
 print("You started work at "+time.ctime())
 while(break_count < total_breaks):
-    time.sleep(10)
+    time.sleep(60*30)
     webbrowser.open("https://www.youtube.com/watch?v=rSg4m1hpEFI")
     break_count += 1
 
