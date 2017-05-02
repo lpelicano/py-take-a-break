5b97b35c3f7c4334438a3d64bede6a21f373b381
lpelicano <lehpelicano@gmail.com>
2017-05-02 14:13:07 +0100

    v1.0

---
+++ README.md
@@ -0,0 +1,8 @@
+Take a Break (v1.0) - A simple script when running forces you to take a break
+
+Utilises webbrowser and time modules
+
+Future tasks
+- Develop GUI for v2.0
+- Customisable break intervals
+- Turn into fully fledged application that is easily executable
--- break.py
+++ break.py
@@ -1,1 +1,13 @@
 import time, webbrowser
+
+
+total_breaks = 3
+break_count = 0
+
+print("You started work at "+time.ctime())
+while(break_count < total_breaks):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=rSg4m1hpEFI")
+    break_count += 1
+
+print("End of session")
